ee693445bcdb52dcd74448d09121545d5f9f2244
Marcos Camacho <marcoscachis@gmail.com>
2023-03-10 05:56:34 -0600

    new ui register page with email

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,7 +1,6 @@
-import 'package:app/widgets/button.dart';
-import 'package:app/widgets/date_select.dart';
+import 'package:app/widgets/buttons/button.dart';
 import 'package:flutter/material.dart';
-import 'package:app/auth.dart';
+import 'package:app/auth/auth.dart';
 import 'package:email_validator/email_validator.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -25,7 +24,9 @@ class _RegisterPageState extends State<RegisterPage> {
           name, email, password, setError);
       if (response == true) {
         //enviar a la página de login
-        Navigator.pushReplacementNamed(context, '/login');
+        Future.delayed(Duration(seconds: 2), () {
+          Navigator.pushReplacementNamed(context, '/login');
+        });
       }
     } else {
       print("no se pudo registrar");
@@ -44,25 +45,23 @@ class _RegisterPageState extends State<RegisterPage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Column(
               children: [
-                const Text("Regístrate",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    )),
-                /*  const Text("Bienvenido a Lockey",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    )), */
                 const Image(
-                  image: AssetImage("assets/lockey-logo.png"),
-                  height: 300,
-                  width: 300,
+                  image: AssetImage("assets/illustrations/illustration9.png"),
+                  height: 250,
+                  width: 240,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                  child: const Text("Regístrate en Lockey",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Playfair',
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      )),
                 ),
                 Form(
                     key: _formKey,
@@ -70,75 +69,116 @@ class _RegisterPageState extends State<RegisterPage> {
                       //space between the elements
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        TextFormField(
-                          onSaved: (value) => name = value!,
-                          decoration: const InputDecoration(
-                            hintText: "Nombre",
-                            labelText: "Escribe tu nombre",
-                            border: OutlineInputBorder(),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey[100],
                           ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "El nombre no puede estar vacío";
-                            }
-                            return null;
-                          },
+                          child: TextFormField(
+                            onSaved: (value) => name = value!,
+                            decoration: const InputDecoration(
+                              hintText: "Nombre",
+                              labelText: "Escribe tu nombre",
+                              prefixIcon: Icon(Icons.person),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "El nombre no puede estar vacío";
+                              }
+                              return null;
+                            },
+                          ),
                         ),
                         const SizedBox(
                           height: 20,
                         ),
-                        TextFormField(
-                          onSaved: (value) => email = value!,
-                          decoration: const InputDecoration(
-                            hintText: "Correo electrónico",
-                            labelText: "Correo electrónico",
-                            border: OutlineInputBorder(),
+                        Center(
+                          child: Container(
+                            color: Colors.grey[100],
+                            child: TextFormField(
+                              onSaved: (value) => email = value!,
+                              decoration: const InputDecoration(
+                                hintText: "Correo electrónico",
+                                labelText: "Correo electrónico",
+                                prefixIcon: Icon(Icons.email),
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  borderSide:
+                                      BorderSide(color: Colors.black, width: 1),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (!EmailValidator.validate(value!)) {
+                                  return "El correo electrónico no es válido";
+                                }
+                                return null;
+                              },
+                            ),
                           ),
-                          validator: (value) {
-                            if (!EmailValidator.validate(value!)) {
-                              return "El correo electrónico no es válido";
-                            }
-                            return null;
-                          },
                         ),
                         const SizedBox(
                           height: 20,
                         ),
-                        TextFormField(
-                          onSaved: (value) => password = value!,
-                          decoration: const InputDecoration(
-                            hintText: "Contraseña",
-                            labelText: "Contraseña",
-                            border: OutlineInputBorder(),
+                        Container(
+                          color: Colors.grey[100],
+                          child: TextFormField(
+                            onSaved: (value) => password = value!,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              hintText: "Contraseña",
+                              labelText: "Contraseña",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value != passwordConfirm) {
+                                return "Las contraseñas no coinciden";
+                              }
+                              if (value!.length < 6 || value.isEmpty) {
+                                return "La contraseña debe tener al menos 6 caracteres";
+                              }
+                              return null;
+                            },
                           ),
-                          validator: (value) {
-                            if (value != passwordConfirm) {
-                              return "Las contraseñas no coinciden";
-                            }
-                            if (value!.length < 6 || value.isEmpty) {
-                              return "La contraseña debe tener al menos 6 caracteres";
-                            }
-                            return null;
-                          },
                         ),
                         const SizedBox(
                           height: 20,
                         ),
-                        TextFormField(
-                          onSaved: (value) => passwordConfirm = value!,
-                          decoration: const InputDecoration(
-                            hintText: "Confirmar contraseña",
-                            labelText: "Confirmar contraseña",
-                            border: OutlineInputBorder(),
+                        Container(
+                          color: Colors.grey[100],
+                          child: TextFormField(
+                            onSaved: (value) => passwordConfirm = value!,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              hintText: "Confirmar contraseña",
+                              labelText: "Confirmar contraseña",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value != password) {
+                                return "Las contraseñas no coinciden";
+                              }
+                              if (value!.length < 6 || value.isEmpty) {
+                                return "La contraseña debe tener al menos 6 caracteres";
+                              }
+                            },
                           ),
-                          validator: (value) {
-                            if (value != password) {
-                              return "Las contraseñas no coinciden";
-                            }
-                            if (value!.length < 6 || value.isEmpty) {
-                              return "La contraseña debe tener al menos 6 caracteres";
-                            }
-                          },
                         ),
                         const SizedBox(
                           height: 20,
@@ -153,21 +193,39 @@ class _RegisterPageState extends State<RegisterPage> {
                             ),
                           ),
                         Container(
-                            margin: EdgeInsets.only(top: 20),
+                            width: 250,
+                            height: 50,
+                            margin: EdgeInsets.only(top: 10),
                             child: ButtonWidget(
                               onPressed: _submit,
                               textContent: "Registrarse",
                             )),
                       ],
                     )),
+                const SizedBox(
+                  height: 5,
+                ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text("¿No tienes una cuenta?"),
+                    const Text(
+                      "Ya tienes una cuenta?",
+                      style: TextStyle(
+                        fontFamily: 'Playfair',
+                        fontSize: 20,
+                      ),
+                    ),
                     TextButton(
                       onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/');
+                        Navigator.pushReplacementNamed(context, '/login');
                       },
-                      child: const Text("Inicia sesión"),
+                      child: const Text(
+                        "Inicia sesión",
+                        style: TextStyle(
+                          fontFamily: 'Playfair',
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
                   ],
                 )
