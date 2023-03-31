8306acd176035e25902c5047dec1aecd35885aee
Marcos Camacho <marcoscachis@gmail.com>
2023-03-31 14:23:34 -0600

    add kitchen page

--- lib/pages/kitchen/kitchen_page.dart
+++ lib/pages/kitchen/kitchen_page.dart
@@ -3,6 +3,7 @@ import 'package:mqtt_client/mqtt_client.dart';
 
 import '../../layouts/room_page.dart';
 import '../../services/mqtt_client_service.dart';
+import '../../widgets/slider/slider_widget.dart';
 
 class KitchenPage extends StatefulWidget {
   const KitchenPage({super.key});
@@ -12,6 +13,10 @@ class KitchenPage extends StatefulWidget {
 }
 
 class _KitchenPageState extends State<KitchenPage> {
+  bool isConnected = false;
+  double lightValue = 0;
+  var changeLightValue;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -24,15 +29,54 @@ class _KitchenPageState extends State<KitchenPage> {
         print("not connected");
       }
       await mqttService.connectMqtt();
+      mqttService.subscribeToTopic("home/kitchen/led");
+
+      clientMqtt.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+        final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+        final String message =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        print("message desde el home: $message");
+        final pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        if (c[0].topic == 'home/kitchen/led') {
+          setState(() {
+            lightValue = double.parse(pt);
+          });
+        }
+      });
+      setState(() {
+        isConnected = true;
+        changeLightValue = (double value) {
+          mqttService.publish("home/kitchen/led", value.toString());
+        };
+      });
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!isConnected) {
+      return Center(child: CircularProgressIndicator());
+    }
     return LockeyLayout(
-        child: Center(
-      child: Text("hola xd"),
-    ));
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(children: [
+          //imagen network
+          Image.network(
+            "https://cdn-icons-png.flaticon.com/512/7627/7627796.png",
+            height: 200,
+            width: 200,
+          ),
+          Text("Luz en la cocina",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold)),
+          SliderWidget(lightValue: lightValue, setlightValue: changeLightValue)
+        ]),
+      ),
+    );
   }
 }
