c06be59ff531d5a94f52a84aa4a0bfe214f45182
Marcos Camacho <marcoscachis@gmail.com>
2023-04-03 12:00:43 -0600

    add lamp icon to slider

--- lib/widgets/slider/slider_widget.dart
+++ lib/widgets/slider/slider_widget.dart
@@ -21,17 +21,23 @@ class SliderWidget extends StatefulWidget {
 class _SliderWidgetState extends State<SliderWidget> {
   @override
   Widget build(BuildContext context) {
-    return Slider(
-      activeColor: Colors.yellow,
-      thumbColor: Colors.white,
-      inactiveColor: Colors.grey,
-      value: widget.lightValue,
-      min: 0,
-      max: 100,
-      divisions: 100,
-      onChanged: (double value) {
-        widget.setlightValue(value);
-      },
+    return Column(
+      children: [
+        Image.network("https://cdn-icons-png.flaticon.com/128/1582/1582439.png",
+            width: 59, height: 50),
+        Slider(
+          activeColor: Colors.yellow,
+          thumbColor: Colors.white,
+          inactiveColor: Colors.grey,
+          value: widget.lightValue,
+          min: 0,
+          max: 100,
+          divisions: 100,
+          onChanged: (double value) {
+            widget.setlightValue(value);
+          },
+        ),
+      ],
     );
   }
 }
